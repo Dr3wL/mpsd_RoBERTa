d8eb2e6fbb4ee554d21fee80e713c849d23d48ee
Dr3wL <93017330+Dr3wL@users.noreply.github.com>
2024-12-02 18:50:44 -0500

    Delete download_rob.py

--- download_rob.py
+++
@@ -1,9 +0,0 @@
-from transformers import RobertaTokenizer, RobertaForSequenceClassification
-
-# Download and save the model and tokenizer locally
-model = RobertaForSequenceClassification.from_pretrained('roberta-base', num_labels=2)
-tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-
-# Save the model and tokenizer to your local directory
-model.save_pretrained('./local_model')
-tokenizer.save_pretrained('./local_model')
